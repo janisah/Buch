9ba4aa6d364b7e57895d29a82e401194020cba5f
Tarantel-Timon <krautimon03@ahfs-sus.de>
2021-04-23 09:36:48 +0200

    aeg

--- Buch.java
+++ Buch.java
@@ -7,5 +7,23 @@
  */
 public class Buch
 {
+    private String titel, autor;
+    private int isbn;
+    public Buch(String titel, String autor, int isbn){
+        this.titel=titel;
+        this.autor=autor;
+        this.isbn=isbn;
+    }
     
+    public String getTitel(){
+        return titel;
+    }
+    
+    public String getAutor(){
+        return autor;
+    }
+    
+    public int getIsbn(){
+        return isbn;
+    }
 }
--- Buchverwaltung.java
+++ Buchverwaltung.java
@@ -7,27 +7,75 @@
  */
 public class Buchverwaltung
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Buchverwaltung
-     */
-    public Buchverwaltung()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+    private Buch buchregal[]=new Buch[5];
+    private Buch tauschen[]=new Buch[1];
+    int speicher;
+    int ende = buchregal.length;
+    int anfang = 0;
+    
+    
+    public void buecherspeichern(){
+        buchregal[0] = new Buch("Bibel", "Gott", 23489);
+        buchregal[1] = new Buch("Herr der Ringe", "J.R.R Tolkien", 56234);
+        buchregal[2] = new Buch("Harry Potter", "J.K.Rowling", 23465);
+        buchregal[3] = new Buch("Ausgerechnet Sylt", "Thomas Herzberg", 11111);
+        buchregal[4] = new Buch("Frühling auf Schottisch", "Karin Lindberg", 34677);
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    public void buechersortierenTitel(){    
+        for(int anfang = 0; anfang < ende; anfang++)
+        {
+            for(int aktuell = 0; aktuell < ende; aktuell++)
+            {      
+                if(buchregal[anfang].getTitel().compareTo(buchregal[aktuell].getTitel())<0)    //Es wird gespeichert wo das Beste ist
+                {
+                      tausche(anfang, aktuell);
+                }
+            }
+        }
+        buecherausgeben();
+    }   
+    
+    public void buechersortierenIsbn(){    
+        for(int anfang = 0; anfang < ende; anfang++)
+        {
+            for(int aktuell = 0; aktuell < ende; aktuell++)
+            {      
+                if(buchregal[anfang].getIsbn() < buchregal[aktuell].getIsbn())    //Es wird gespeichert wo das Beste ist
+                {
+                      tausche(anfang, aktuell);
+                }
+            }
+        }
+        buecherausgeben();
+    }   
+    
+    public void buechersortierenAutor(){    
+        for(int anfang = 0; anfang < ende; anfang++)
+        {
+            for(int aktuell = 0; aktuell < ende; aktuell++)
+            {      
+                if(buchregal[anfang].getAutor().compareTo(buchregal[aktuell].getAutor())<0)    //Es wird gespeichert wo das Beste ist
+                {
+                      tausche(anfang, aktuell);
+                }
+            }
+        }
+        buecherausgeben();
+    } 
+    
+    public void buecherausgeben(){
+        for(int i = 0; i < buchregal.length;i++){
+            System.out.println(buchregal[i].getTitel());
+        } 
     }
+
+    public void tausche(int zahl1, int zahl2){
+        
+        tauschen[0] = buchregal[zahl1];
+        buchregal[zahl1] = buchregal[zahl2];
+        buchregal[zahl2] = tauschen[0];
+    }
+    
+    
 }
